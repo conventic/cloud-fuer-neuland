b0cffc1f16ad42768eb7c3f1aeef80a039cc642b
flheck <f.hecking@conventic.com>
2021-06-08 16:18:23 +0200

    Add: Snackbar

--- src/components/skeleton/Layout.tsx
+++ src/components/skeleton/Layout.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import Helm from "./Helm";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { Snackbar } from "../Snackbar";
 import { QueryClientProvider, QueryClient } from "react-query";
 
 import "../../sass/main.scss";
+import { SnackbarProvider } from "../../context/snackbar-ctx";
 
 const TemplateWrapper = ({ children }: any) => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Helm /><div className="site">
-      <Navbar />
-
-        <main className="site__content">{children}</main>
-     
-      <Footer /> </div>
+      <SnackbarProvider>
+        <Helm />
+        <div className="site">
+          <Navbar />
+          <main className="site__content">{children}</main>
+          <Snackbar />
+          <Footer />
+        </div>
+      </SnackbarProvider>
     </QueryClientProvider>
   );
 };
